b84c67c4a0fdbe0c698022b4039856ca4de01381
tkutcher <tim@tkutcher.com>
2025-09-07 14:56:34 -0400

    refactor: Refactor distribution list reading

--- src/jcvb/newsletter.py
+++ src/jcvb/newsletter.py
@@ -2,6 +2,7 @@ import csv
 import datetime
 import logging
 import os
+import pathlib
 
 import markdown
 import sendgrid
@@ -11,7 +12,7 @@ from jcvb._consts import JCVB_PUBLIC
 from jcvb._consts import JCVB_ROOT
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
@@ -19,12 +20,6 @@ _NEWSLETTERS_DIR = JCVB_ROOT / "newsletters"
 _SENT_NEWSLETTERS_DIR = JCVB_PUBLIC / "newsletters"
 _DISTRIBUTION_LIST_CSV = _NEWSLETTERS_DIR / "distribution-list.csv"
 _NEXT_NEWSLETTER_PATH = _NEWSLETTERS_DIR / "Next-Newsletter.md"
-
-
-def _read_distribution_list_csv():
-    with open(_DISTRIBUTION_LIST_CSV, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)[1:]
 
 
 def _read_next_newsletter_md() -> str:
@@ -42,37 +37,63 @@ def _file_newsletter_as_sent(date: datetime.date) -> None:
         f.write(_read_next_newsletter_md())
 
 
-def _send_newsletter_to_emails(sg: sendgrid.SendGridAPIClient, to_emails) -> None:
+def _send_newsletter_to_emails(
+    sg: sendgrid.SendGridAPIClient,
+    to_email: str,
+    recipients: list[tuple[str, str]],
+    file_as_sent=False,
+) -> None:
     today = datetime.date.today()
     message = sendgrid.Mail(
         from_email=sendgrid.Email("jcvb@tkutcher.com"),
-        to_emails=to_emails,
+        to_emails=[sendgrid.To(email=to_email)],
         subject=f"🏐JC Volleyball {today.strftime('%m/%d')} Newsletter",
         html_content=_read_next_newsletter_html(),
     )
+    for recipient in recipients:
+        message.add_bcc(sendgrid.Bcc(email=recipient[1], name=recipient[0]))
     response = sg.client.mail.send.post(request_body=message.get())
     logging.info(f"Sent newsletter - SendGrid Response {response.status_code}")
+    if file_as_sent:
+        _file_newsletter_as_sent(today)
 
 
 class NewsletterDistributor:
-    def __init__(self, sg: sendgrid.SendGridAPIClient, dry_run_email: str) -> None:
+    def __init__(
+        self,
+        sg: sendgrid.SendGridAPIClient,
+        to_email: str,
+        distribution_list_path: pathlib.Path = _DISTRIBUTION_LIST_CSV,
+    ) -> None:
         self._sg = sg
-        self._dry_run_email = dry_run_email
-        self._distribution = _read_distribution_list_csv()
+        self._to_email = to_email
+        self._distribution_list_path = distribution_list_path
 
-    def dry_run(self):
-        _send_newsletter_to_emails(self._sg, self._dry_run_email)
+    def distribute_newsletter(self, file_as_sent=True):
+        _send_newsletter_to_emails(
+            self._sg,
+            to_email=self._to_email,
+            recipients=self._read_distribution_list(),
+            file_as_sent=file_as_sent,
+        )
 
-    # noinspection PyMethodMayBeStatic
-    def file_as_sent(self):
-        _file_newsletter_as_sent(datetime.date.today())
+    def _read_distribution_list(self) -> list[tuple[str, str]]:
+        to_emails: list[tuple[str, str]] = []
+        with open(self._distribution_list_path, "r") as f:
+            reader = csv.reader(f)
+            next(reader)
+            for name, email in reader:
+                to_emails.append((name, email))
+        return to_emails
 
 
 if __name__ == "__main__":
     load_dotenv()
     distributor = NewsletterDistributor(
         sg=sendgrid.SendGridAPIClient(api_key=os.environ.get("TK_SG_API_KEY")),
-        dry_run_email="tkutcher@outlook.com",
+        to_email="tkutcher@johncarroll.org",
+        distribution_list_path=_DISTRIBUTION_LIST_CSV,
     )
-    distributor.dry_run()
-    distributor.file_as_sent()
+    distributor.distribute_newsletter(
+        file_as_sent=True,
+    )
